ce65be3539f4f1599302559e81f77a12fd4edc75
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-01 13:06:10 -0400

    Update FilterDriver.cpp

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -9,8 +9,8 @@
 
 using namespace std;
 
-void swap1(vector<BaseballStatistic>&, int, int);
-void swap2(vector<BaseballStatistic>&, int, int);
+void swap1(vector<BaseballStatistic>&, string, char);
+void swap2(vector<BaseballStatistic>&, string, int);
 void sort1(vector<BaseballStatistic>&);
 void sort2(vector<BaseballStatistic>&);
 int search(vector<BaseballStatistic>, string, string);
@@ -41,12 +41,31 @@ void loadFile(vector<BaseballStatistic>& e_Stat){
 	}
 }
 
-void swap(vector<BaseballStatistic>& e_Stat, int a, int b);
+void swap1(vector<BaseballStatistic>& e_Stat, string a, char b);
   {
-    
-    int temp = *a;
+    string temp = *a;
     *a=*b;
     *b =temp;
   }
-
+void sort1(vector<BaseballStatistic>& e_Stat){
+    
+     int i, j, min_idx; 
+   
+    for (i = 0; i < e_Stat.size()-1; i++) 
+    { 
+        min_idx = i; 
+        
+        for (j = i+1; j < e_Stat.size(); j++) 
+          if (
+              (e_Stat[j].getLastName()const.compare(e_Stat[min_idx].getLastName()const) < 0)
+              
+              || (e_Stat[j].getLastName()const.compare(e_Stat[min_idx].getLastName()const) == 0 && e_Stat[j].getFisrtName()const.compare(e_Stat[min_idx].getFirstName()const ) < 0)
+              
+              || (e_Stat[j].getLastName()const.compare(e_Stat[min_idx].getLastName()const) == 0 && e_Stat[j].getFirstName()const.compare(e_Stat[min_idx].getFirstName()const) == 0 && e_Stat[j].getPostion()const < e_Stat[min_idx].getPosition()const ) 
+              )
+            min_idx = j; 
+      
+        swap1(e_Stat, min_idx, i);
+         } 
+}
 
