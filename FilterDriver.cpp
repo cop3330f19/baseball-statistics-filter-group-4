6a78196c9a129ec8a2a513c0f7198d03f0e04668
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-01 12:25:31 -0400

    Update FilterDriver.cpp

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -1,1 +1,42 @@
+#include "BaseballStatistic.h"
+#include "Data.h"
+#include "StringHelper.h"
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
 
+using namespace std;
+
+void swap(vector<BaseballStatistic>&, int, int);
+void sort(vector<BaseballStatistic>&);
+int search(vector<BaseballStatistic>, string, string);
+void loadFile(vector<BaseballStatistic>&);
+void printHeading();
+
+int main(){
+  
+  
+}
+
+
+
+
+
+void loadFile(vector<BaseballStatistic>& e_Stat){
+	ifstream in;
+	in.open("BStats.csv");
+	
+	std::string firstName, lastName, teamName, position;
+	int jerseyNum, atBats, hr, rbi, sb;
+	char batting, throwing;
+	double battingAverage, ops,era;
+  int birthDay, birthMonth, birthYear; 
+	
+	while(teamName >> jerseyNum >> firstNmae >> lastName >> birthYear >> birthMonth >> birthDay >> batting >> throwing >> atBats >> battingAverage >> hr >> rbi >> sb >> ops >> era >> position){
+		
+		BaseballStatistic temp(teamName, jerseyNum, firstNmae, lastName, birthYear, birthMonth, birthDay, batting, throwing, atBats, battingAverage, hr, rbi, sb, ops, era, position);
+		e_Stat.push_back(temp);
+	}
+}
