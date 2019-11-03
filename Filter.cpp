bef63b73daa445a37a00a7fa3c53b34fe7f836a9
jermainelegrand <57329006+jermainelegrand@users.noreply.github.com>
2019-11-03 16:13:48 -0500

    Update Filter.cpp

--- Filter.cpp
+++ Filter.cpp
@@ -1,1 +1,16 @@
+#include "Filter.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+void sort1(vector<BaseballStatistic>&)
+{
+  
+}
+
+void search(vector<BaseballStatistic>, string, string)
+{
+  
+}
