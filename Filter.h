5e921160c308421270e48e6a8298fcbce077e64a
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-03 15:55:27 -0500

    Update Filter.h

--- Filter.h
+++ Filter.h
@@ -1,1 +1,14 @@
+#ifndef SORTSEARCH
+#define SORTSEARCH
 
+#include <string>
+#include <vector>
+
+class SortSearch{
+  
+  public:
+          void sort1(vector<BaseballStatistic>&);
+          void sort2(vector<BaseballStatistic>&);
+          void search(vector<BaseballStatistic>, string, string);
+  
+}          
