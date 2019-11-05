61caa2c0b351169a3bd62d3c27c5e0ee728f3cd4
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-04 19:16:35 -0500

    Update FilterDriver.cpp

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -1,6 +1,7 @@
 #include "BaseballStatistic.h"
-#include "Data.h"
+#include "Date.h"
 #include "StringHelper.h"
+#include "Filter.h"
 #include <fstream>
 #include <vector>
 #include <iostream>
@@ -9,13 +10,14 @@
 
 using namespace std;
 
+void loadFile(vector<BaseballStatistic>&);
 void swap1(vector<BaseballStatistic>&, int, int);
 void swap2(vector<BaseballStatistic>&, int, int);
-void sort1(vector<BaseballStatistic>&);
-void sort2(vector<BaseballStatistic>&);
-int search1(vector<BaseballStatistic>, string, string);
-int search2(vector<BaseballStatistic>, string);
-void loadFile(vector<BaseballStatistic>&);
+void sort1(vector<BaseballStatistic>& e_Stat);
+void sort2(vector<BaseballStatistic>& e_Stat);
+int search1(vector<BaseballStatistic> e_Stat, string, string);
+int search2(vector<BaseballStatistic> e_Stat, string);
+
 
 int main(){ 
  // Use cases to prompt user to choose from sorting options. 
@@ -29,11 +31,11 @@ int main(){
 	switch(toupper(option))
 	{
 		case 'a':
-			sort1(vector<BaseballStatistic>&);
-			search1(vector<BaseballStatistic>e_Stat, lastName, firstName);
+			sort1(vector<BaseballStatistic>& e_Stat);
+			search1(vector<BaseballStatistic>& e_Stat, string firstName, string lastName);
 		case 'b':
-			sort2(vector<BaseballStatistic>&);
-			search2(vector<BaseballStatistic>e_Stat, teamName);
+			sort2(vector<BaseballStatistic>& e_Stat);
+			search2(vector<BaseballStatistic>& e_Stat, string teamName);
 		default:
 			cout << "You did not select a correct option." << endl;
 			break;
@@ -75,98 +77,10 @@ void swap1(vector<BaseballStatistic>& e_Stat, int a, int b);
     *a=*b;
     *b =temp;
   }
-void sort1(vector<BaseballStatistic>& e_Stat){
-    
-     int i, j, min_idx; 
-   
-    for (i = 0; i < e_Stat.size()-1; i++) 
-    { 
-        min_idx = i; 
-        
-        for (j = i+1; j < e_Stat.size(); j++) 
-          if (
-              (e_Stat[j].getLastName()const.compare(e_Stat[min_idx].getLastName()const) < 0)
-              
-              || (e_Stat[j].getLastName()const.compare(e_Stat[min_idx].getLastName()const) == 0 && e_Stat[j].getFirstName()const.compare(e_Stat[min_idx].getFirstName()const ) < 0)
-              
-              || (e_Stat[j].getLastName()const.compare(e_Stat[min_idx].getLastName()const) == 0 && e_Stat[j].getFirstName()const.compare(e_Stat[min_idx].getFirstName()const) == 0 && e_Stat[j].getPosition()const < e_Stat[min_idx].getPosition()const ) 
-              )
-            min_idx = j; 
-      
-        swap1(e_Stat, min_idx, i);
-         } 
+
 void swap2(vector<BaseballStatistic>& e_Stat, int a, int b);
   {
     BaseballStatistic temp = *a;
     *a=*b;
     *b =temp;
   }
-	
-void sort2(vector<BaseballStatistic>& e_Stat)
-{
-	   int i, j, min_idx; 
-   
-    for (i = 0; i < e_Stat.size()-1; i++) 
-    { 
-        min_idx = i; 
-        
-        for (j = i+1; j < e_Stat.size(); j++) 
-          if (
-              (e_Stat[j].getTeamName()const.compare(e_Stat[min_idx].getTeamName()const) < 0)
-              
-              || (e_Stat[j].getTeamName()const.compare(e_Stat[min_idx].getTeamName()const) == 0 && e_Stat[j].getJerseyNum()const.compare(e_Stat[min_idx].getJerseyNum()const ) < 0)
-    )
-		  min_idx = j; 
-      
-        swap2(e_Stat, min_idx, i);
-         } 
-
-int search1(vector<BaseballStatistic> e_Stat, string firstName, string lastName){
-  
-  int lo = 0;
-  int hi = e_Stat.size();
-  
-   while (lo <= hi) 
-    { 
-        int location = lo + (hi-lo)/2; 
-	   
-        if (e_Stat[location].getLastName() == lastName && e_Stat[location].getFirstName() == firstName) 
-            return location; 
-  
-    
-        if (e_Stat[location].getLastName() < lastName 
-			|| (e_Stat[location].getLastName() == lastName && e_Stat[location].getFirstName() < firstName)) 
-            lo = location + 1; 
-  
-        else
-            hi = location - 1; 
-    } 
-  
-    return -1; 
- 
-}
-
-int search2(vector<BaseballStatistic> e_Stat, string teamName){
-  
-  int lo = 0;
-  int hi = e_Stat.size();
-  
-   while (lo <= hi) 
-    { 
-        int location = lo + (hi-lo)/2; 
-	   
-        if (e_Stat[location].getTeamName() == teamName ) 
-            return location; 
-  
-    
-        if (e_Stat[location].getTeamName() < teamName 
-			|| (e_Stat[location].getTeamName() == teamName))
-            lo = location + 1; 
-  
-        else
-            hi = location - 1; 
-    } 
-  
-    return -1; 
- 
-}
