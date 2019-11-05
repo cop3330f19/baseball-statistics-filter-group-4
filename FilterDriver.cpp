6532684764e73ab298a218fb2e2dd41483323bc3
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-05 02:29:05 -0500

    Update FilterDriver.cpp

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -11,15 +11,14 @@
 using namespace std;
 
 void loadFile(vector<BaseballStatistic>&);
-/*void swap1(vector<BaseballStatistic>&, int, int);
-void swap2(vector<BaseballStatistic>&, int, int);
-void sort1(vector<BaseballStatistic>& e_Stat);
-void sort2(vector<BaseballStatistic>& e_Stat);
-int search1(vector<BaseballStatistic> e_Stat, std::string, std::string);
-int search2(vector<BaseballStatistic> e_Stat, std::string);
-*/
+
+
 
 int main(){ 
+  
+  Filter obj;
+    
+ 
     
     
  // Use cases to prompt user to choose from sorting options. 
@@ -30,25 +29,29 @@ int main(){
     
     loadFile(e_Stat);
 	
+    
 	cout << "Would you like to search by A) Player Name & Position, or B) Team & Jersey Number? Enter A or B: ";
 	cin >> option;
 	
 	switch(option)
 	{
 		case 'A':
-            cout << teamName;
-			//sort1(e_Stat);
-			//search1(e_Stat, firstName, lastName);
-			break;
+            cout << "Pre Sorted List" << endl;
+	        for(int i = 0; i < e_Stat.size(); i++)
+		    e_Stat[i].print();
+            //loadFile(e_Stat);
+			obj.sort1(e_Stat);
+			obj.search1(e_Stat, firstName, lastName);
+            break;
 		case 'B':
-			//sort2(e_Stat);
-			//search2(e_Stat, teamName);
+			obj.sort2(e_Stat);
+			obj.search2(e_Stat, teamName);
 			break;
 		default:
 			cout << "You did not select a correct option." << endl;
 			break;
-	}
-	
+	 }
+    
 	
 			
 
@@ -67,16 +70,72 @@ void loadFile(vector<BaseballStatistic>& e_Stat)
 	char batting, throwing;
 	double battingAverage, ops,era;
     int day, month, year;
-    while(in){
-	//string str;
     
-	//parse(str, ',');
-	
-	in >> teamName >> jerseyNum >> firstName >> lastName >> year >> month >> day >> batting >> throwing >> atBats >> battingAverage >> hr >> rbi >> sb >> ops >> era >> position;
-	
-	BaseballStatistic temp(firstName, lastName, teamName, jerseyNum, position, year, month, day, batting, throwing, atBats, battingAverage, hr, rbi, sb, ops, era);
-	e_Stat.push_back(temp);
+    
+    
+    while(in.good())
+    {
+       // deccaring temp variable as a string
+        string temp;
+        //read in line then slit by delimiter
+        getline(in, temp, ',');
+       teamName = temp;
+        getline(in, temp,',');
+       //convert the string to an int
+       stringstream loading(temp);
+       //set converted variable to corresponding variable 
+       loading >> jerseyNum;
+        getline(in, temp, ',');
+       firstName = temp;
+        getline(in, temp, ',');
+       lastName = temp;
+        getline(in, temp, ',');
+       stringstream loading2(temp);
+       loading2 >> year;
+        getline(in, temp, ',');
+       stringstream loading3(temp);
+       loading3 >> month;
+        getline(in, temp, ',');
+       stringstream loading4(temp);
+       loading4 >> day;
+        getline(in, temp, ',');
+       //create a char variable of the same length of the read in variable
+       char p[temp.length()];
+       //set the temp char variable to the read in variable
+       p[0] = temp[0];
+       //set the temp char variable to the corresponding variable
+       batting = p[0];
+        getline(in, temp, ',');
+       p[0] = temp[0];
+       throwing = p[0];
+        getline(in, temp, ',');
+       stringstream loading5(temp);
+       loading5 >> atBats;
+        getline(in, temp, ',');
+       stringstream loading6(temp);
+       loading6 >> battingAverage;
+        getline(in, temp, ',');
+       stringstream loading7(temp);
+       loading7 >> hr;
+        getline(in, temp, ',');
+       stringstream loading8(temp);
+       loading8 >> rbi;
+        getline(in, temp, ',');
+       stringstream loading9(temp);
+       loading9 >> sb;
+        getline(in, temp, ',');
+       stringstream loading10(temp);
+       loading10 >> ops;
+        getline(in, temp, ',');
+       stringstream loading11(temp);
+       loading11 >> era;
+        getline(in, temp);
+       position = temp;
+        
+        
+       // cout << teamName << " " << jerseyNum << " " << firstName << " " << lastName << " " << year << " " << month << " " << day << " " << batting << " " << throwing << " " << atBats << " " << battingAverage << " " << hr << " " << rbi << " " << sb << " " << ops << " " << era << " " << position << endl;
+       BaseballStatistic temp1(firstName, lastName, teamName, jerseyNum, position, year, month, day, batting, throwing, atBats, battingAverage, hr, rbi, sb, ops, era);
+	e_Stat.push_back(temp1);
 	}
-}
-
-
+     in.close();
+    }
