0d0172bc929905a47108cc2f2ba7619763e6d1fa
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-04 22:44:56 -0500

    Update FilterDriver.cpp

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -11,31 +11,39 @@
 using namespace std;
 
 void loadFile(vector<BaseballStatistic>&);
-void swap1(vector<BaseballStatistic>&, int, int);
+/*void swap1(vector<BaseballStatistic>&, int, int);
 void swap2(vector<BaseballStatistic>&, int, int);
 void sort1(vector<BaseballStatistic>& e_Stat);
 void sort2(vector<BaseballStatistic>& e_Stat);
-int search1(vector<BaseballStatistic> e_Stat, string, string);
-int search2(vector<BaseballStatistic> e_Stat, string);
-
+int search1(vector<BaseballStatistic> e_Stat, std::string, std::string);
+int search2(vector<BaseballStatistic> e_Stat, std::string);
+*/
 
 int main(){ 
+    
+    
  // Use cases to prompt user to choose from sorting options. 
        vector<BaseballStatistic> e_Stat;
        loadFile(e_Stat);
 	char option;
+    string firstName, lastName, teamName;
+    
+    loadFile(e_Stat);
 	
 	cout << "Would you like to search by A) Player Name & Position, or B) Team & Jersey Number? Enter A or B: ";
 	cin >> option;
 	
-	switch(toupper(option))
+	switch(option)
 	{
-		case 'a':
-			sort1(vector<BaseballStatistic>& e_Stat);
-			search1(vector<BaseballStatistic>& e_Stat, string firstName, string lastName);
-		case 'b':
-			sort2(vector<BaseballStatistic>& e_Stat);
-			search2(vector<BaseballStatistic>& e_Stat, string teamName);
+		case 'A':
+            cout << teamName;
+			//sort1(e_Stat);
+			//search1(e_Stat, firstName, lastName);
+			break;
+		case 'B':
+			//sort2(e_Stat);
+			//search2(e_Stat, teamName);
+			break;
 		default:
 			cout << "You did not select a correct option." << endl;
 			break;
@@ -49,38 +57,26 @@ int main(){
 
 
 
-void loadFile(vector<BaseballStatistic>& e_Stat){
+void loadFile(vector<BaseballStatistic>& e_Stat)
+{
 	ifstream in;
 	in.open("BStats.csv");
 	
-	while(in){
-	string str;
-	parse(str, ',');
-		
 	std::string firstName, lastName, teamName, position;
 	int jerseyNum, atBats, hr, rbi, sb;
 	char batting, throwing;
 	double battingAverage, ops,era;
-        int birthDay, birthMonth, birthYear;
+    int day, month, year;
+    while(in){
+	//string str;
+    
+	//parse(str, ',');
 	
-	in >> teamName >> jerseyNum >> firstName >> lastName >> birthYear >> birthMonth >> birthDay >> batting >> throwing >> atBats >> battingAverage >> hr >> rbi >> sb >> ops >> era >> position;
+	in >> teamName >> jerseyNum >> firstName >> lastName >> year >> month >> day >> batting >> throwing >> atBats >> battingAverage >> hr >> rbi >> sb >> ops >> era >> position;
 	
-		
-	BaseballStatistic temp(teamName, jerseyNum, firstName, lastName, birthYear, birthMonth, birthDay, batting, throwing, atBats, battingAverage, hr, rbi, sb, ops, era, position);
+	BaseballStatistic temp(firstName, lastName, teamName, jerseyNum, position, year, month, day, batting, throwing, atBats, battingAverage, hr, rbi, sb, ops, era);
 	e_Stat.push_back(temp);
 	}
 }
 
-void swap1(vector<BaseballStatistic>& e_Stat, int a, int b);
-  {
-    BaseballStatistic temp = *a;
-    *a=*b;
-    *b =temp;
-  }
 
-void swap2(vector<BaseballStatistic>& e_Stat, int a, int b);
-  {
-    BaseballStatistic temp = *a;
-    *a=*b;
-    *b =temp;
-  }
