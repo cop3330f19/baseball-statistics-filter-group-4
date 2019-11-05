f69b370006e021b8524f7cf8b05f853421c8a5ac
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-05 02:30:20 -0500

    Update Filter.h

--- Filter.h
+++ Filter.h
@@ -11,11 +11,9 @@ class Filter{
           void sort2(std::vector<BaseballStatistic>&);
           int search1(std::vector<BaseballStatistic>, std::string, std::string);
           int search2(std::vector<BaseballStatistic>, std::string);
-          void swap1(std::vector<BaseballStatistic>&, int, int);
-          void swap2(std::vector<BaseballStatistic>&, int, int);
+          void swap(std::vector<BaseballStatistic>&, int, int);
   
 };        
 
 #endif
-
 
