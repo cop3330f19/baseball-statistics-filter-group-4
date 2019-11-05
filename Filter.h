fdd655ed5aaac3550078923a0a398e2390c4ebe7
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-11-04 19:14:56 -0500

    Update Filter.h

--- Filter.h
+++ Filter.h
@@ -1,16 +1,18 @@
-#ifndef SORTSEARCH
-#define SORTSEARCH
-
+#ifndef FILTER
+#define FILTER
 #include <string>
 #include <vector>
+#include "BaseballStatistic.h"
 
-class SortSearch{
+class Filter{
   
   public:
-          void sort1(vector<BaseballStatistic>&);
-          void sort2(vector<BaseballStatistic>&);
+          void sort1(vector<BaseballStatistic>& e_Stat);
+          void sort2(vector<BaseballStatistic>& e_Stat);
           int search1(vector<BaseballStatistic>, string, string);
           int search2(vector<BaseballStatistic>, string);
   
-};         
+};        
+
 #endif
+
